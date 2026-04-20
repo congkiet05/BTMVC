402ca042e5c79d8f3a9e24adfff39aa6176c2140
congkiet05 <congkiet05@gmail.com>
2026-04-20 09:57:06 +0700

    add Course.cs

---
+++ WebApplication1/WebApplication1/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public ICollection<ClassSection> ClassSections { get; set; }
+    }
+}
