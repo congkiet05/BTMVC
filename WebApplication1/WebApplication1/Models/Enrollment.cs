09a6e7b2a7d52bc422d82262d661c8c26f5b8c64
congkiet05 <congkiet05@gmail.com>
2026-04-20 10:00:31 +0700

    add Enrollment.cs

---
+++ WebApplication1/WebApplication1/Models/Enrollment.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentId { get; set; }
+
+        // FK
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+
+        public int ClassSectionId { get; set; }
+        public ClassSection ClassSection { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+    }
+}
