96407784964e466de4bd14cbb07d1df5973810a0
congkiet05 <congkiet05@gmail.com>
2026-04-20 09:58:48 +0700

    add Student.cs

---
+++ WebApplication1/WebApplication1/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
