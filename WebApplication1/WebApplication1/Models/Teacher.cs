5a7c5484cdcfb59d3f4d5cf2c73baf1579485ef1
congkiet05 <congkiet05@gmail.com>
2026-04-20 09:57:47 +0700

    add Teacher.cs

---
+++ WebApplication1/WebApplication1/Models/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ClassSection> ClassSections { get; set; }
+    }
+}
